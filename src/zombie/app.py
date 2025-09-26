ebed4368d5bf8fd59abfe5561e809ca6573c6b1f
Dan Birman <danbirman@gmail.com>
2025-09-26 14:18:10 -0700

    refactor: clearing up zombie for new MVP

--- src/zombie/app.py
+++
@@ -1,9 +0,0 @@
-""" Zoomable Observatory for Multiscale Brain Investigation and Exploration """
-
-import panel as pn
-from hdmf_zarr import NWBZarrIO
-import altair as alt
-import pandas as pd
-
-
-pn.extension('vega')
--- src/zombie/database.py
+++
@@ -1,6 +0,0 @@
-"""Zombe core database module"""
-
-from param import Parameterized
-
-class ZombieDatabase(Parameterized):
-    """Dataframe class for the main Zombie database."""
