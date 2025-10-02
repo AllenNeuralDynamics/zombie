2dd4919fbfc3d78f74e2cefcd8c029aa325923e5
Dan Birman <danbirman@gmail.com>
2025-10-01 22:00:12 -0700

    feat: pulling start/end times from loader to create zoom element

--- src/zombie/app_contents/time_view.py
+++ src/zombie/app_contents/time_view.py
@@ -3,11 +3,25 @@ import panel as pn
 from panel.custom import PyComponent
 from zombie.layout import OUTER_STYLE
 
+from zombie.settings.loader_settings import loader_settings
+
 
 class TimeView(PyComponent):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        
+        loader_settings.param.watch(self._start_time_changed, 'start_time')
+        loader_settings.param.watch(self._end_time_changed, 'end_time')
+
+    def _init_panels(self):
+        pass
+
+    def _start_time_changed(self, event):
+        print(f"Start time changed to: {event.new}")
+    
+    def _end_time_changed(self, event):
+        print(f"End time changed to: {event.new}")
 
     def __panel__(self):
         return pn.Column(
